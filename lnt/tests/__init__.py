ce265fecf4838298e6db61ff33fbca570d43ba18
Dave Rigby <daver@couchbase.com>
2016-09-26 16:27:59 +0100

    Add watson branches to memcached & ep_engine testsuites

--- lnt/tests/__init__.py
+++ lnt/tests/__init__.py
@@ -5,7 +5,11 @@ Access to built-in tests.
 # FIXME: There are better ways to do this, no doubt. We also would like this to
 # be extensible outside of the installation. Lookup how 'nose' handles this.
 
-known_tests = {'memcached', 'ep_engine'}
+known_tests = {'memcached',
+               'memcached_watson',
+               'ep_engine',
+               'ep_engine_watson',
+}
 
 def get_test_names():
     """get_test_names() -> list
